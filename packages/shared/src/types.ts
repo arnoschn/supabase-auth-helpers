fa13a17385711659cf633fd06a2977e05db6a6c7
arnoschn <arnoschn@users.noreply.github.com>
2023-03-16 11:04:22 +0100

    Update types.ts

--- packages/shared/src/types.ts
+++ packages/shared/src/types.ts
@@ -3,7 +3,7 @@ import type { SupabaseClientOptions } from '@supabase/supabase-js';
 
 export type CookieOptions = { name?: string } & Pick<
   CookieSerializeOptions,
-  'domain' | 'secure' | 'path' | 'sameSite' | 'maxAge'
+  'domain' | 'secure' | 'path' | 'sameSite' | 'maxAge' | 'expires'
 >;
 
 export type SupabaseClientOptionsWithoutAuth<T = 'public'> = Omit<
